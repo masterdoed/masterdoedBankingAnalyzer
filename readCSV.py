6b6cfaca5fed7cab450e1fb4b34682db1b8568e0
masterdoed <masterdoed@doedspace.de>
2021-01-07 16:34:48 +0100

    initial commit

---
+++ readCSV.py
@@ -0,0 +1,34 @@
+import csv
+import pandas
+from tabulate import tabulate
+
+#CAMT to CSV Format
+#Auftragskonto
+#Buchungstag
+#Valutadatum
+#Buchungstext
+#Verwendungszweck
+#Glaeubiger ID
+#Mandatsreferenz
+#Kundenreferenz (End-to-End)
+#Sammlerreferenz
+#Lastschrift Ursprungsbetrag
+#Auslagenersatz Ruecklastschrift
+#Beguenstigter/Zahlungspflichtiger
+#Kontonummer/IBAN
+#BIC (SWIFT-Code)
+#Betrag
+#Waehrung
+#Info
+
+
+df=pandas.read_csv('test.csv',encoding='ISO-8859-1', error_bad_lines=False, sep=';', decimal=',')
+df['Betrag']=pandas.to_numeric(df['Betrag'])
+df['Betrag']=df['Betrag'].astype(float)
+
+summe=0
+for index,row in df.iterrows():
+    summe=summe + row['Betrag']
+    print(row['Buchungstag'] + "|" +  row['Auftragskonto'] + "|" +  row['Verwendungszweck'] + "|" + str(row['Betrag']))
+
+print(summe)
